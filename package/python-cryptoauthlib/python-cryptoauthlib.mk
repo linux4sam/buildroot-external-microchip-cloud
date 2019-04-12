a73cd0ff1266c6ca162cf41677bee50d72bd4008
Larry Gass <larry.gass@microchip.com>
2019-04-12 16:23:53 -0700

    added more packages to defconfig, upgraded cryptoauthlib

--- package/python-cryptoauthlib/python-cryptoauthlib.mk
+++ package/python-cryptoauthlib/python-cryptoauthlib.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_CRYPTOAUTHLIB_VERSION = 20190105
+PYTHON_CRYPTOAUTHLIB_VERSION = 20190304
 PYTHON_CRYPTOAUTHLIB_SOURCE = cryptoauthlib-$(PYTHON_CRYPTOAUTHLIB_VERSION).tar.gz
-PYTHON_CRYPTOAUTHLIB_SITE = https://pypi.python.org/packages/cf/f3/779e3c01c5592500219fe7300f8095ede130abab027a2ff0158f4834c769
+PYTHON_CRYPTOAUTHLIB_SITE = https://pypi.python.org/packages/a6/b9/698260893ad00c0a730012c1ef7e22c7dc214d219af790069a57a48fbf0f
 PYTHON_CRYPTOAUTHLIB_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
